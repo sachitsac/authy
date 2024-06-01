cec4c7d9a6b0a9c3fc3dcb33cf75f9d2a4e1940d
Sachit Malhotra <sachitsac@gmail.com>
2024-06-02 00:24:47 +1000

    feat: Upsert user to store a local reference

--- apps/central-auth/src/app/api/auth/[auth0]/route.ts
+++ apps/central-auth/src/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,35 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import {
+  Session,
+  handleAuth,
+  handleLogin,
+  handleCallback,
+} from "@auth0/nextjs-auth0";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+const afterCallback = async (req: NextRequest, session: Session) => {
+  try {
+    await prisma.user.upsert({
+      where: { email: session.user.email },
+      create: {
+        auth0Id: session.user.sub,
+        email: session.user.email,
+        lastSeen: new Date().toISOString(),
+      },
+      update: {
+        lastSeen: new Date().toISOString(),
+      },
+    });
+  } catch (error) {
+    console.error("User already exists", error);
+  }
+  return session;
+};
 
 export const GET = handleAuth({
+  callback: handleCallback({ afterCallback }),
   login: handleLogin({
     returnTo: "/dashboard",
   }),
